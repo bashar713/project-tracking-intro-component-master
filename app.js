075d752b7ed7c2ffe1df0ea4eb7aee783af5faf6
bashar713 <bashar050abo@gmail.com>
2021-10-19 15:08:41 +0300

    Upload files , this site is not ready

---
+++ app.js
@@ -0,0 +1,10 @@
+//! Force Scroll to the Top of the Page on Page Reload//
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+//!----------------//
+
